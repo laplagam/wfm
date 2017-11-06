ebce9e0f80cd93bf1fdc4b4c58e5b1feea5ead28
laplagam <laplagam@gmail.com>
2017-11-06 21:40:10 +0100

    Applied main view to the index page. Added comments to index code.

--- index.php
+++ index.php
@@ -4,17 +4,34 @@ ini_set('display_errors','1');
 
 require_once('matchclass.php');
 require_once('clubclass.php');
+require_once('views/viewmain.php');
 
+//Let's create Chelsea
 $chelsea = new ClubClass();
 $chelsea->makeTeam('Chelsea FC',200);
 
+//Let's create Newcastle
 $newcastle = new ClubClass();
 $newcastle->makeTeam('Newcastle FC',180);
 
+//Let's create a match. 
 $match = new MatchClass();
 
+//Load Chelsea and Newcastle as opponents. 
 $match->loadTeams($chelsea,$newcastle);
+
+//Let's run the match. 
 $match->runMatch();
+
+//Let's create the main view. 
+$mainview = new vMainView();
+
+//Let's add the header to the site
+echo $mainview->makeHeaderView();
+
+//Let's display the match result log. 
 $match->printResult();
 
+//Let's add the footer to the site
+echo $mainview->makeFooterView();
 ?>
