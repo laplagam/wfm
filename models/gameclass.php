7b1b2464f97ac69def8d060d27d0ae5c04dce666
laplagam <laplagam@gmail.com>
2017-11-16 18:53:52 +0100

    Created GameClass. Made create game function with query to make insert game data.

---
+++ models/gameclass.php
@@ -0,0 +1,43 @@
+<?php
+
+class GameClass
+{
+  //TODO: Create log file to store everytime an exception happens
+  var $pdo;
+  var $errormessage='';
+  var $errorcount = 0;
+  function __construct(PdoConnection $pdo)
+  {
+    $this->pdo = $pdo;
+  }
+
+  function createGame($userid,$leagueid,$clubid,$gamename)
+  {
+    $dbh = $this->pdo;
+    $query = 'INSERT INTO tblusergame(userid,leagueid,clubid,gamename) VALUES (:userid,:leagueid,:clubid,:gamename)';
+    $stmt = $dbh->prepare($query);
+
+    $stmt->bindParam(':userid',$userid,PDO::PARAM_INT);
+    $stmt->bindParam(':leagueid',$leagueid,PDO::PARAM_INT);
+    $stmt->bindParam(':clubid',$clubid,PDO::PARAM_INT);
+    $stmt->bindParam(':gamename',$gamename,PDO::PARAM_STR);
+
+    try
+    {
+      $stmt->execute();
+    }
+    catch(Exception $e)
+    {
+      $this->errormessage = $e->getMessage();
+      $this->errorcount++;
+      echo 'Failed to create a new game. ';
+    }
+    
+  }
+  function loadGame($gameid)
+  {
+
+  }
+}
+
+?>
