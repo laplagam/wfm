668e97813635660141c2be4f63ccf9aa84e12fdc
laplagam <laplagam@gmail.com>
2017-11-18 22:53:37 +0100

    Added function to get league table data.

--- models/tableclass.php
+++ models/tableclass.php
@@ -2,9 +2,28 @@
 
 class TableClass
 {
-  function __construct()
+  var $pdo;
+  function __construct(PdoConnection $pdo)
   {
+
+  }
+
+  function getTableData($gameid=6)
+  {
+    $dbh = $this->pdo->getPdoCon();    
+    $query = 'SELECT leagueid,teamid, teamname, matchesplayed, goalsfor, 
+      goalsagainst, victory, draw, loss, points, season, userid, gameid 
+      FROM tblusertable 
+      WHERE gameid=:gameid';
+
+    $stmt = $dbh->prepare($query);
+    $stmt->bindParam(':gameid',$gameid,PDO::PARAM_INT);
     
+    $stmt->execute();
+
+    $result = $stmt->fetchAll();
+
+    return $result;
   }
 }
 
